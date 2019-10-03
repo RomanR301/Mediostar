4f9722aaf11c689a22224ac3259912ed4e3d31fe
Roman Savchuk <roman.sav15@gmail.com>
2019-10-03 11:31:28 +0300

    test on mobile

--- src/js/roman.js
+++ src/js/roman.js
@@ -1,15 +1,15 @@
 // Roman
 $(document).ready(function() {
   // FAQ Disabled button
-  $('#faq-pib, #faq-phone, #faq-email, #faq-message').on("keyup", action);
+  // $('#faq-pib, #faq-phone, #faq-email, #faq-message').on("keyup", action);
 
-  function action() {
-    if( $('#faq-pib').val().length > 0 && $('#faq-phone').val().length > 0 && $('#faq-email').val().length > 0 && $('#faq-message').val().length > 0) {
-      $('.contact-submit').removeClass('btn-disabled').addClass('btn-primary')
-    } else {
-      $('.contact-submit').removeClass('btn-primary').addClass('btn-disabled')
-    }   
-  }
+  // function action() {
+  //   if( $('#faq-pib').val().length > 0 && $('#faq-phone').val().length > 0 && $('#faq-email').val().length > 0 && $('#faq-message').val().length > 0) {
+  //     $('.contact-submit').removeClass('btn-disabled').addClass('btn-primary')
+  //   } else {
+  //     $('.contact-submit').removeClass('btn-primary').addClass('btn-disabled')
+  //   }   
+  // }
 
   // Single specialist certificate popup 
   $('.certificates-item, .gallery-item-link ').magnificPopup({
@@ -46,15 +46,16 @@ $(document).ready(function() {
     }, 600);
   });
 
-  $('.inner-hamburger').click(function(){
 
-    let innerHamburger = $('.hamburger-lines');
-    let innerNav = $('.inner-nav-menu');
+  let innerHamburger = $('.hamburger-lines');
+  let innerNav = $('.inner-nav-menu');
 
+  $('.inner-hamburger, .inner-nav-close').click(function(){
     innerHamburger.hasClass("open") ? (innerHamburger.removeClass("open"),
     innerNav.toggleClass("active")) : (innerHamburger.addClass("open"),
     innerNav.toggleClass("active"))
   })
+
 
   var sidebar = new StickySidebar('#sidebar', {
     containerSelector: '#main-content',
