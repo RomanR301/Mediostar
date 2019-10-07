e36a4b569f343fb22a5ee7b2a98dc0f6c5e28f89
Roman Savchuk <roman.sav15@gmail.com>
2019-10-07 13:14:13 +0300

    contacts modal

--- src/js/roman.js
+++ src/js/roman.js
@@ -51,17 +51,84 @@ $(document).ready(function() {
   let innerNav = $('.inner-nav-menu');
 
   $('.inner-hamburger, .inner-nav-close').click(function(){
+    $('body').toggleClass('preventScrolling');
     innerHamburger.hasClass("open") ? (innerHamburger.removeClass("open"),
     innerNav.toggleClass("active")) : (innerHamburger.addClass("open"),
     innerNav.toggleClass("active"))
   })
 
-
-  var sidebar = new StickySidebar('#sidebar', {
-    containerSelector: '#main-content',
-    innerWrapperSelector: '.sidebar__inner',
-    topSpacing: 150,
-    bottomSpacing: 80,
-    minWidth: 768,
+  $(".inner-nav-menu li.-hasSubMenu .services-dropdown").on("click", function(t) {
+    t.preventDefault(),
+    $(this).parent().parent().hasClass("isOpen") ? $(this).parent().parent().removeClass("isOpen") : ($(".inner-nav-menu li.-hasSubMenu").removeClass("isOpen"),
+    $(this).parent().parent().addClass("isOpen")),
+    $(".inner-nav-menu li.-hasSubMenu").each(function(t) {
+        $(".inner-nav-menu li.-hasSubMenu").eq(t).hasClass("isOpen") ? $(".inner-nav-menu li.-hasSubMenu").eq(t).find(".services-subnav").slideToggle() : $(".inner-nav-menu li.-hasSubMenu .services-subnav").eq(t).slideUp()
+    })
   });
-});
+
+  $('.modal').hide();
+  $('.contacts-main-button').click(function() {
+      $('body').css('overflow', 'hidden');
+      $('.modal').show();
+  });
+  $('.js-close-modal').click(function() {
+      $('.modal').hide();
+      $('body').css('overflow', 'visible');
+  });
+
+
+
+
+
+  // let modal = {
+  //   closeButton: $('.js-close-modal'),
+  //   closeOverlay: $('.modal'),
+
+  //   init: function () {
+  //       this.events();
+  //   },
+  //   openModal: function (id) {
+  //       let modalWindow = $(id);
+  //       modalWindow.fadeIn();
+  //       modalWindow.find('.modal-content').removeClass('animate-away').addClass('animate-in');
+  //       $('html,body').addClass('js-modal');
+  //       $('html,body').off('scroll mousewheel touchmove');
+  //   },
+
+  //   closeModal: function (id) {
+  //       let modalWindow = $(id);
+  //       modalWindow.find('.modal-content').removeClass('animate-in').addClass('animate-away');
+  //       modalWindow.fadeOut();
+  //       $('html,body').removeClass('js-modal');
+  //       $('html,body').on('scroll mousewheel touchmove');
+  //   },
+  //   events: function () {
+
+  //     $(document).on('click', '.modalTrigger', function (e) {
+  //         e.preventDefault();
+  //         let self = $(this),
+  //         target = self.attr('data-modal');
+  //         modal.openModal(target);
+  //     });
+
+  //     $(document).on('click', '.modal', function (event) {
+  //         let self = '#' + $(this).attr('id');
+  //         if (event.target.className == 'modal-body') {
+  //             modal.closeModal(self);
+  //         }
+  //     });
+
+  //     $(document).on('click', '.js-close-modal', function () {
+  //         let self = '#' + $(this).closest('.modal').attr('id');
+  //         modal.closeModal(self);
+  //     });
+
+  //   }
+  // }
+  // jQuery(function () {
+  //   frontend.init();
+  //   slider.init();
+  //   modal.init();
+  // });
+
+}); 
