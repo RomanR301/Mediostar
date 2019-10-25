98720e4f84e524c2212610fce3439dcffc5166f8
Roman Savchuk <roman.sav15@gmail.com>
2019-10-25 14:33:21 +0300

    minor fixes

--- src/js/roman.js
+++ src/js/roman.js
@@ -156,20 +156,22 @@ $(document).ready(function() {
       });
     });
 
-    function readURL(input) {
-      if (input.files && input.files[0]) {
-          var reader = new FileReader();
-          reader.onload = function(e) {
-              $('.imagePreview').css('background-image', 'url('+e.target.result +')');
-              $('.imagePreview').hide();
-              $('.imagePreview').fadeIn(650);
-          }
-          reader.readAsDataURL(input.files[0]);
-      }
-    }
-    $(".form-file").change(function() {
-        readURL(this);
-    });
+    // function readURL(input) {
+    //   if (input.files && input.files[0]) {
+    //       var reader = new FileReader();
+    //       reader.onload = function(e) {
+    //           $('.imagePreview').css('background-image', 'url('+e.target.result +')');
+    //           $('.imagePreview').hide();
+    //           $('.imagePreview').fadeIn(650);
+    //       }
+    //       reader.readAsDataURL(input.files[0]);
+    //   }
+    // }
+    // $(".form-file").change(function() {
+    //     readURL(this);
+    // });
+
+
     // function readURL(input) {
     //   if (input.files && input.files[0]) {
     //     var reader = new FileReader();
